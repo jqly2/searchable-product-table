6af40f0b4862c19c3c9b2b2813a082e22030744a
Jonathan Ly <jonathanly213@gmail.com>
2024-01-16 23:12:37 -0800

    added config, tsx changes and gitignore.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1>
+                New App
+            </h1>
+            <p>
+                Edit @ <code>src/App.tsx</code> 
+            </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
